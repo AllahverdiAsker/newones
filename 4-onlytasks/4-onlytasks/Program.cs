c356440cc0e40f89e9bdc046537ce2a092d844e1
Allahverdi Asgarli <allahverdiaa@code.edu.az>
2022-06-13 01:45:56 -0700

    newones

--- 4-onlytasks/4-onlytasks/Program.cs
+++ 4-onlytasks/4-onlytasks/Program.cs
@@ -7,25 +7,31 @@ namespace _4_onlytasks
         static void Main(string[] args)
         {
             int[] numbers = { 3, 9, };
-            int[] numbers1 = { 3, 5, 7, 8, 4, 3, 2, 5, 6, 7 };
+            int[] numbers1 = { 3, 5, 7, 8, 4, 3, 2, 5, 6, 7 ,9};
             SameNumbersInEachArray(ref numbers, ref numbers1);
             Console.WriteLine(SameNumbersInEachArray(ref numbers, ref numbers1));
 
             static bool SameNumbersInEachArray(ref int[] numbers, ref int[] numbers1)
             {
-
+                bool check = false;
              for(int i = 0; i < numbers.Length; i++)
                 {
                     for(int o=0;o<numbers1.Length;o++)
                     {
                         if (numbers[i] == numbers1[o])
                         {
-                            return true;
-                        }
+                            check = true;
+                            break;
 
+
+                        }
+                        else check = false;
+
+                        
                     }
                 }
-                return false;
+                return check;
+                
             }
         }
     }
